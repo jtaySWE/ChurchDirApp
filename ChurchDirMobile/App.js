e55afac15f67db802000b53fa9fcbab00cf66480
Jason Tay <jtay0034@student.monash.edu>
2024-02-23 23:41:53 +1100

    setted up screen for member signing up

--- ChurchDirMobile/App.js
+++ ChurchDirMobile/App.js
@@ -1,11 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useController, useForm } from 'react-hook-form';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+
+const Input = ({name, control, placeholder}) => {
+  const {field} = useController({
+    control,
+    defaultValue: '',
+    name
+  })
+  return (
+    <TextInput 
+      value={field.value}
+      onChangeText={field.onChange}
+      placeholder={placeholder} 
+      style={styles.input}
+      />
+    )
+}
 
 export default function App() {
+  const {control, handleSubmit} = useForm()
+  const onSubmit = (data) => {
+
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Welcome!</Text>
+      <Input name="givenName" control={control} placeholder="Given name"/>
+      <Input name="surname" control={control} placeholder="Surname"/>
+      <Input name="email" control={control} placeholder="Email"/>
+      <Input name="phone" control={control} placeholder="Phone"/>
+      <Input name="address" control={control} placeholder="Address"/>
       <StatusBar style="auto" />
+      <Button 
+      title="Submit"
+      onPress={handleSubmit(onSubmit)}
+      />
     </View>
   );
 }
@@ -16,5 +47,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  },input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  }
 });
