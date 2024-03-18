962cd7fa0e3d85da293f6ddbdcaae289b107e7a1
Jason Tay <jtay0034@student.monash.edu>
2024-03-18 18:45:50 +1100

    made the logout button

--- ChurchDirAPI/APIBackend/obj/Debug/net8.0/APIBackend.AssemblyInfo.cs
+++ ChurchDirAPI/APIBackend/obj/Debug/net8.0/APIBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APIBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7ebf1e864eb0795af9e55a48d4103009c81529a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb4098b31db9e205974cc6fac8125f4558815e6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("APIBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APIBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ChurchDirMobile/App.js
+++ ChurchDirMobile/App.js
@@ -1,10 +1,11 @@
-import { Keyboard, SafeAreaView, StyleSheet, TouchableWithoutFeedback } from 'react-native';
+import { Button, Keyboard, SafeAreaView, StyleSheet, TouchableWithoutFeedback } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Member from './Screens/Member';
 import MemberList from './Screens/MemberList';
 import Login from './Screens/Login';
 import React from 'react';
+import { SimpleLineIcons } from '@expo/vector-icons';
 
 function MemberScreen() {
   return (<Member isSignUp={false}/>)
@@ -18,8 +19,13 @@ export default function App() {
 
   const Tab = createBottomTabNavigator()
   const [isSignedIn, setSignIn] = React.useState(false)
+  
   const loginMember = () => {
     setSignIn(true)
+  }
+
+  const logoutMember = () => {
+    setSignIn(false)
   }
 
   return (
@@ -27,8 +33,16 @@ export default function App() {
       <SafeAreaView style={styles.safeContainer}>
         {isSignedIn ? 
         <NavigationContainer>
-          <Tab.Navigator initialRouteName='Home'>
-            <Tab.Screen name='Home' component={MemberScreen}/>
+          <Tab.Navigator 
+          initialRouteName='Home'
+          >
+            <Tab.Screen name='Home' component={MemberScreen}
+            options={
+              {
+                headerRight: () => (<SimpleLineIcons.Button name='logout' 
+                  onPress={logoutMember} color="black" backgroundColor="white"/>)
+              }
+            }/>
             <Tab.Screen name='Members' component={MemberListScreen}/>
           </Tab.Navigator>
         </NavigationContainer> : 
