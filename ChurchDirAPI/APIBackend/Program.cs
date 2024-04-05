e882bf61c24f5472b71f919af811cde08267ce3a
Jason Tay <jtay0034@student.monash.edu>
2024-04-05 18:17:36 +1100

    Installed AWS lambda for deploying backend

--- ChurchDirAPI/APIBackend/Program.cs
+++ ChurchDirAPI/APIBackend/Program.cs
@@ -1,11 +1,9 @@
-using Newtonsoft.Json.Serialization;
 using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.DataModel;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -16,6 +14,12 @@ var awsOptions = builder.Configuration.GetAWSOptions();
 builder.Services.AddDefaultAWSOptions(awsOptions);
 builder.Services.AddAWSService<IAmazonDynamoDB>();
 builder.Services.AddScoped<IDynamoDBContext, DynamoDBContext>();
+
+builder.Services.AddHttpsRedirection(options =>
+{
+    options.RedirectStatusCode = 307;
+    options.HttpsPort = 5001;
+});
 
 var app = builder.Build();
 
