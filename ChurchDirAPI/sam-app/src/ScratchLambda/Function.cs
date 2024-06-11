94da9f9f548d3a6ba456d12f0bbe645e3e596905
Jason Tay <jasontay2013@gmail.com>
2024-06-11 16:15:53 +1000

    deployed sam template

--- ChurchDirAPI/sam-app/src/ScratchLambda/Function.cs
+++ ChurchDirAPI/sam-app/src/ScratchLambda/Function.cs
@@ -1,7 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Nodes;
+using System.Text.Json;
 using System.Threading.Tasks;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using ScratchLambda.Models;
 
 using Amazon.Lambda.Core;
 
@@ -12,19 +19,161 @@ namespace ScratchLambda;
 
 public class Function
 {
-    
+
     /// <summary>
-    /// A simple function that takes a string and does a ToUpper
+    /// Gets all members
     /// </summary>
     /// <param name="input"></param>
     /// <param name="context"></param>
     /// <returns></returns>
-    public string FunctionHandler(string input, ILambdaContext context)
+    public async Task<APIGatewayHttpApiV2ProxyResponse> FunctionHandler(APIGatewayHttpApiV2ProxyRequest request, ILambdaContext context)
     {
-        var message = input ?? "Hello from Lambda!";
-        // All log statements are written to CloudWatch by default. For more information, see
-        // https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-logging.html
-        context.Logger.LogLine($"Processed message: {message}");
-        return message.ToUpper();
+        AmazonDynamoDBClient client = new AmazonDynamoDBClient();
+        DynamoDBContext dbContext = new DynamoDBContext(client);
+        string userID = null;
+        var members = await dbContext.ScanAsync<Member>(default).GetRemainingAsync();
+        return new APIGatewayHttpApiV2ProxyResponse
+        {
+            Body = JsonSerializer.Serialize(members),
+            StatusCode = 200
+        };
+        // Get user ID if in path parameter
+        /*if (request.PathParameters != null)
+        {
+            if (request.PathParameters.ContainsKey("userID"))
+            {
+                userID = request.PathParameters["userID"];
+            }
+        }
+
+        if (request.RouteKey.Contains("GET /AllMembers"))
+        {
+            // Get all members
+            var members = await dbContext.ScanAsync<Member>(default).GetRemainingAsync();
+            return new APIGatewayHttpApiV2ProxyResponse
+            {
+                //Headers = respHeader,
+                Body = JsonSerializer.Serialize(members),
+                StatusCode = 200
+            };
+
+        }
+        else if (request.RouteKey.Contains("GET /Member") && userID != null)
+        {
+            Console.WriteLine($"Getting member of ID {userID}");
+            var member = await dbContext.LoadAsync<Member>(userID, userID);
+
+            // Make sure this member is already in the database
+            if (member == null)
+            {
+                return new APIGatewayHttpApiV2ProxyResponse
+                {
+                    //Headers = respHeader,
+                    Body = $"The member with user ID {userID} does not exists.",
+                    StatusCode = 400
+                };
+            }
+
+            return new APIGatewayHttpApiV2ProxyResponse
+            {
+                //Headers = respHeader,
+                Body = JsonSerializer.Serialize(member),
+                StatusCode = 200
+            };
+        }
+        else if (request.RouteKey.Contains("POST /Member") && request.Body != null)
+        {
+            var newMember = JsonSerializer.Deserialize<Member>(request.Body);
+            var member = await dbContext.LoadAsync<Member>(newMember.PK, newMember.SK);
+
+            // Make sure this member is not already in the database, so that we can add new member
+            if (member != null)
+            {
+                return new APIGatewayHttpApiV2ProxyResponse
+                {
+                    Body = $"The member with user ID {newMember.PK} already exists.",
+                    StatusCode = 400
+                };
+            }
+            await dbContext.SaveAsync(newMember);
+
+            return new APIGatewayHttpApiV2ProxyResponse
+            {
+                Body = JsonSerializer.Serialize(newMember),
+                StatusCode = 201
+            };
+        }
+        else if (request.RouteKey.Contains("POST /SignIn") && request.Body != null)
+        {
+            JsonNode newMember = JsonNode.Parse(request.Body);
+            var member = await dbContext.LoadAsync<Member>(newMember["userID"].GetValue<string>());
+
+            // Make sure this member is already in the database
+            if (member == null)
+            {
+                return new APIGatewayHttpApiV2ProxyResponse
+                {
+                    Body = "Incorrect user ID.",
+                    StatusCode = 400
+                };
+            }
+
+            return new APIGatewayHttpApiV2ProxyResponse
+            {
+                Body = JsonSerializer.Serialize(member),
+                StatusCode = 201
+            };
+        }
+        else if (request.RouteKey.Contains("DELETE /Member") && userID != null)
+        {
+            var member = await dbContext.LoadAsync<Member>(userID);
+
+            // Make sure this member is already in the database
+            if (member == null)
+            {
+                return new APIGatewayHttpApiV2ProxyResponse
+                {
+                    Body = $"The member with user ID {userID} does not exists.",
+                    StatusCode = 400
+                };
+            }
+
+            await dbContext.DeleteAsync(member);
+            return new APIGatewayHttpApiV2ProxyResponse
+            {
+                //Headers = respHeader,
+                Body = $"Member with user ID {member.UserID} removed successfully",
+                StatusCode = 200
+            };
+        }
+        else if (request.RouteKey.Contains("PUT /Member") && request.Body != null)
+        {
+            var currMember = JsonSerializer.Deserialize<Member>(request.Body);
+            var member = await dbContext.LoadAsync<Member>(currMember.PK, currMember.SK);
+
+            // Make sure this member is in the database, to modify his/her data
+            if (member == null)
+            {
+                return new APIGatewayHttpApiV2ProxyResponse
+                {
+                    Body = $"The member with user ID {currMember.PK} does not exists.",
+                    StatusCode = 400
+                };
+            }
+            await dbContext.SaveAsync(currMember);
+
+            return new APIGatewayHttpApiV2ProxyResponse
+            {
+                //Headers = respHeader,
+                Body = JsonSerializer.Serialize(currMember),
+                StatusCode = 200
+            };
+        }
+
+        return new APIGatewayHttpApiV2ProxyResponse
+        {
+            Body = "Bad Request",
+            StatusCode = 400
+        };*/
     }
 }
---
+++ ChurchDirAPI/sam-app/src/ScratchLambda/Models/Member.cs
@@ -0,0 +1,31 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+
+namespace ScratchLambda.Models;
+
+[DynamoDBTable("ChurchMemberProd")]
+public class Member
+{
+    [DynamoDBHashKey("pk")]
+    public string? PK { get; set; }
+
+    [DynamoDBRangeKey("sk")]
+    public string? SK { get; set; }
+
+    [DynamoDBProperty("userID")]
+    public string? UserID { get; set; }
+
+    [DynamoDBProperty("givenName")]
+    public string? GivenName { get; set; }
+
+    [DynamoDBProperty("surname")]
+    public string? Surname { get; set; }
+
+    [DynamoDBProperty("email")]
+    public string? Email { get; set; }
+
+    [DynamoDBProperty("phone")]
+    public string? Phone { get; set; }
+
+    [DynamoDBProperty("address")]
+    public string? Address { get; set; }
+}
